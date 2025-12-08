e8ef9e39bef95b0bdf494bea72fedc74b4e0d10b
darkosima1983 <darkosima1983@gmail.com>
2025-12-08 13:15:10 +0100

    Added cities seeder

---
+++ database/seeders/CitiesSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\CitiesModel;
+class CitiesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Factory::create();
+
+        for ($i=0; $i<100; $i++){
+            CitiesModel::create([
+                "name"=>$faker->city
+            ]);
+        }
+    }
+}
